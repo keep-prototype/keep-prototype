419b6920beacf1a9daf9215ff0745e080e8591e9
ganggyunggyu <qwzx16@naver.com>
2025-03-06 21:47:06 +0900

    chore: 아이콘 추가

---
+++ src/shared/icons/UserProfileIcon.jsx
@@ -0,0 +1,33 @@
+export const UserProfileIcon = () => {
+  return (
+    <svg
+      width="81"
+      height="80"
+      viewBox="0 0 81 80"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_2_591)">
+        <rect
+          x="0.25"
+          width="80"
+          height="80"
+          rx="32"
+          fill="#77E0A7"
+          fillOpacity="0.25"
+        />
+        <path
+          d="M10.2499 65.7625C10.2499 56.9259 17.4133 49.7625 26.2499 49.7625H54.2499C63.0864 49.7625 70.2499 56.9259 70.2499 65.7625V93.7625C70.2499 102.599 63.0864 109.762 54.2499 109.762H26.2499C17.4133 109.762 10.2499 102.599 10.2499 93.7625V65.7625Z"
+          fill="#44A67D"
+          fillOpacity="0.75"
+        />
+        <circle cx="40.2499" cy="28" r="16" fill="#44A67D" fillOpacity="0.75" />
+      </g>
+      <defs>
+        <clipPath id="clip0_2_591">
+          <rect x="0.25" width="80" height="80" rx="32" fill="white" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
