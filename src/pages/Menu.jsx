3c5e1802f878e07d488ad4e15bf002eb4f3319e8
ganggyunggyu <qwzx16@naver.com>
2025-03-07 15:59:34 +0900

    feat: 메뉴 페이지 제작

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,3 +1,45 @@
+import { useNavigate } from 'react-router';
+import { RightButtonIcon } from '../shared/icons/RightButtonIcon';
+
 export const Menu = () => {
-  return <main>menu</main>;
+  const navigate = useNavigate();
+  const submitLogout = () => {
+    removeItem('userName');
+    removeItem('aptName');
+    removeItem('aptDong');
+    removeItem('aptHo');
+
+    navigate('/');
+  };
+  const redirectMyReservation = () => {
+    navigate('/profile/my-reservation');
+  };
+  return (
+    <main>
+      <section className="h-20 flex items-center justify-center">
+        <img
+          className="h-full"
+          src="https://room-critic-s3.s3.ap-northeast-2.amazonaws.com/KEE_BIT_LOGO.png"
+          alt=""
+        />
+      </section>
+      <section className="">
+        <figure
+          onClick={redirectMyReservation}
+          className="border-b border-neutral-300 flex items-center justify-between pl-3 pr-4 py-5"
+        >
+          <button>내 예약</button>
+          <RightButtonIcon />
+        </figure>
+        <figure className="border-b border-neutral-300 flex items-center justify-between pl-3 pr-4 py-5">
+          <button onClick={submitLogout}>내 정보</button>
+          <RightButtonIcon />
+        </figure>
+        <figure className="border-b border-neutral-300 flex items-center justify-between pl-3 pr-4 py-5">
+          <button onClick={submitLogout}>로그아웃</button>
+          <RightButtonIcon />
+        </figure>
+      </section>
+    </main>
+  );
 };
